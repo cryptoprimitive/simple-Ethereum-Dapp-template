68b6136f367590d62b63915ee09f32dc7c39ed22
oglog <coinop.logan@gmail.com>
2018-07-06 17:48:13 +0000

    removing unnecessary components

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
-$(document).ready(function(){
-var loadScrollbar = false;
-
+$(document).ready(function() {
   web3.version.getNetwork((err, netId) => {
     if (netId != "1") {
       $(".warnings").show();
@@ -30,9 +28,10 @@ var loadScrollbar = false;
   });
 
 
-
   resize();
   $(window).resize(resize());
+});
+
   function resize(){
     if (!window.matchMedia("(max-width: 700px)").matches) {
       //main scrollbars
@@ -46,48 +45,3 @@ var loadScrollbar = false;
       });
     }
   }
-
-
-  window.addEventListener("contracts_loaded", () =>{
-    //user info
-    vueInstance.userAddress = web3.eth.accounts[0];
-    web3.eth.getBalance(vueInstance.userAddress , (err, res) =>{
-      vueInstance.userBalance = res;
-    });
-
-    ///fetching the events
-    var eventPromiseArray =[];
-    for(var i=0; i<window.loadedContracts.length; i++){
-      var eventPromise = new Promise((resolve, reject)=>{
-        loadedContracts[i].getAllEvents().then((events) =>{
-          events.forEach((element)=> {
-          vueInstance.events.push(element);
-          });
-          resolve();
-        });
-      });
-      eventPromiseArray.push(eventPromise);
-    }
-    Promise.all(eventPromiseArray).then(function() {
-      //once all events are loaded we can sort the events by timestamp
-      //and load the scrollbar
-      vueInstance.events.sort((a, b) => {
-        return a.timestamp - b.timestamp;
-      });
-      loadEventsScrollbar();
-    });
-  });
-});
-
-//scrollbar for the events is loaded once all events are loaded
-function loadEventsScrollbar() {
-  //scrollbar
-  $('.scrollBar').mCustomScrollbar({
-    scrollButtons: {
-      enable:true
-    },
-    theme:"inset-dark",
-    scrollInertia:150,
-    autoHideScrollbar:true,
-  });
-}
